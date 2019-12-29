95a835cabf5b3d54d1dfaf87afa31fadaff86701
kabeh2 <mattcnk@hotmail.com>
2019-12-28 20:27:15 -0500

    Updates

---
+++ precache-manifest.e33a5840966b366812a7bf28dd501885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8227aa1276aba254ec481403fe52fd2",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "13d0346d695034b41cb6",
+    "url": "/react-shopping-cart/static/css/main.44b93962.chunk.css"
+  },
+  {
+    "revision": "964bd42fb8b5cf7a1a78",
+    "url": "/react-shopping-cart/static/js/2.ab49b950.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-shopping-cart/static/js/2.ab49b950.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13d0346d695034b41cb6",
+    "url": "/react-shopping-cart/static/js/main.815a4bab.chunk.js"
+  },
+  {
+    "revision": "8b28a720c9eeecc2bb2e",
+    "url": "/react-shopping-cart/static/js/runtime-main.3f136478.js"
+  }
+]);
