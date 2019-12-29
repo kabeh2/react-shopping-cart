3762d1a2dac2644d80bffe12fbede4de655330d9
kabeh2 <mattcnk@hotmail.com>
2019-12-28 23:17:21 -0500

    gh pages setup

---
+++ build/precache-manifest.10d70d50787ed4d32377ecbaecd1f9a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6ce4f9230f60f6525ddaf41bf2a4c7",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "13d0346d695034b41cb6",
+    "url": "/shopping-cart/static/css/main.44b93962.chunk.css"
+  },
+  {
+    "revision": "964bd42fb8b5cf7a1a78",
+    "url": "/shopping-cart/static/js/2.ab49b950.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/shopping-cart/static/js/2.ab49b950.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13d0346d695034b41cb6",
+    "url": "/shopping-cart/static/js/main.815a4bab.chunk.js"
+  },
+  {
+    "revision": "13437d0b965bfbfef91f",
+    "url": "/shopping-cart/static/js/runtime-main.276815e1.js"
+  }
+]);
